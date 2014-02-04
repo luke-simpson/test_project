b727154c7c4571809bad67a6a61f0a1e98a485a1
luke.simpson <luke.simpson@liveality.com>
2014-02-03 20:39:15 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestProject::Application.config.secret_key_base = 'de72725e0308eebae0ca2a2d0803a3624da5ac7786771e078b5519bdfe8435d7dac8df6cb0b49b9d0f87a692c1554bdf37bae847a4248925b503e037d186b253'
